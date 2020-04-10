c7eea942ef55f9171f1194469ea0ee8e2768a72b
Meryem Kömür <meryemkmr36@gmail.com>
2020-04-10 09:57:42 -0500

    save storage

--- redux-template/src/index.js
+++ redux-template/src/index.js
@@ -1,32 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//Import redux
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers/rootReducer';
+//router 
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import App from './App';
-import Container from './components/Container'
-// Import Redux
-import {createStore} from 'redux'
-import {Provider} from 'react-redux'
-import rootReducer from './reducers/rootReducer'
-import BaseLayout from './components/layout/BaseLayout'
-
-// router
-import {BrowserRouter,Switch, Route} from 'react-router-dom';
-
-
-let store = createStore( rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-
-
+import BaseLayout from './components/layout/BaseLayout';
+import Container from './components/Container';
+let saveToLocalStorage = (state) => {
+  try {
+    const serializeState = JSON.stringify(state);
+    localStorage.setItem('state', serializeState)
+    //localStorage["state"] = serializeState
+  }
+  catch (e) {
+    console.log(e);
+  }
+}
+let loadFromLocalStorage = () => {
+  try {
+    const serializeState = localStorage.getItem('state');
+    if (serializeState === null) {
+      return undefined
+    }
+    else {
+      return JSON.parse(serializeState)
+    }
+  }
+  catch (e) {
+    console.log(e);
+    return undefined;
+  }
+}
+const persistedState = loadFromLocalStorage()
+let store = createStore(rootReducer,
+  persistedState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+store.subscribe(() => {
+  saveToLocalStorage(store.getState())
+})
 ReactDOM.render(
-  <Provider store = {store}>
+  <Provider store={store}>
     <BrowserRouter>
-     <BaseLayout>
+      <BaseLayout>
         <Switch>
-          <Route exact path ="/" component = {App}/>
-          <Route exact path ="/container" component = {Container}/>
+          <Route exact path="/" component={App} />
+          <Route exact path="/container" component={Container} />
         </Switch>
-      </BaseLayout> 
+      </BaseLayout>
     </BrowserRouter>
   </Provider>
   ,
   document.getElementById('root')
 );
-
